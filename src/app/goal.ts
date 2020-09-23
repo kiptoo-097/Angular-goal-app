1e56f4b78be8747da9bddb61cf791b70feaa5b5c
kiptoo-097 <kiptoo097@gmail.com>
2020-09-24 00:51:20 +0300

    initial commit

---
+++ src/app/goal.ts
@@ -0,0 +1,19 @@
+// export class Goal {
+//     // id: number;
+//     // name:string;
+//     // description:string;
+
+
+// //     showDescription:boolean;
+// //     constructor(public id:number,public name:string,public description:string){
+// //         this.showDescription=false;
+// //     }
+// // }
+
+
+export class Goal {
+    public showDescription: boolean;
+    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+      this.showDescription=false;
+    }
+  }
